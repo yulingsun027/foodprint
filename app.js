10a062d2264275d21ac7241190c7a6d4cf86afdc
Joseph Krassenstein <joseph.krassenstein@gmail.com>
2020-08-15 09:40:57 +0800

    Intro Page Carousel

---
+++ app.js
@@ -0,0 +1,28 @@
+//app.js
+App({
+  onLaunch: function () {
+
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login,
+     wx.getUserInfo,
+     wx.requestPayment)
+    let clientID = '98ad8501ff3f35324114'  // 应用名称: foodprint
+    wx.BaaS.init(clientID)
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    wx.BaaS.auth.loginWithWechat().then(user => {
+      wx.setStorageSync('userInfo', user.toJSON());
+      this.globalData.userInfo = user.toJSON();
+      console.log('current user login',user)
+    }, err => {
+      console.log('fail login')
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/landing/landing.js
@@ -0,0 +1,43 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  data: {
+    currentUser: [],
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    movies:[  
+      {url:'../images/Frame_1.jpg'}, 
+      {url:'../images/Frame_2.jpg'}, 
+      {url:'../images/Frame_3.jpg'}, 
+      ]  
+  },
+  
+  userInfoHandler: function(data){
+    wx.BaaS.auth.loginWithWechat(data).then(user =>{
+      app.globalData.userInfo = user;
+      wx.setStorageSync('userInfo', user);
+      this.setData({
+        currentUser: user
+      })
+    });
+    wx.switchTab({
+      url: '/pages/profile/profile',
+    })
+  },
+
+  onLoad: function () {
+    this.setData({
+      currentUser: app.globalData.userInfo,
+    });
+  },
+  
+  onShow: function () {
+    this.setData({
+      currentUser: app.globalData.userInfo,
+    });
+
+  },
+});
